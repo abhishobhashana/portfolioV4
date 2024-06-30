4a1119e18637badfa0108a2897aa0b0413152a00
abhi <abhishob9099@gmail.com>
2024-06-30 21:11:45 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import data from "./Data/Data.json";
 import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <div className="App flex flex-col bg-light-white-third dark:bg-default font-sans tracking-tight">
       <BrowserRouter>
-        <Helmet>
-          <title>{data.app}</title>
-          <meta name="description" content={data.home.intro}></meta>
-        </Helmet>
         <Layout />
       </BrowserRouter>
     </div>
